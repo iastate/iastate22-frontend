7124ac8906b7d91bf98070438f7669ae805748b6
Brad Walker <brad.walker@idfive.com>
2022-03-09 09:20:50 -0500

    updated hover state for video-embed component

--- src/js/components/video-embed.ts
+++ src/js/components/video-embed.ts
@@ -18,6 +18,7 @@ export class VideoEmbed {
     this.createVideoPlayer();
     this.createPlayPauseButton();
     this.handlePlayPauseButtonClick();
+    this.handlePlayPauseButtonHover();
     this.handlePlayerEvents();
   }
 
@@ -31,6 +32,15 @@ export class VideoEmbed {
   private handlePlayPauseButtonClick() {
     this.playPauseButton.addEventListener("click", () => {
       this.player.playVideo();
+    });
+  }
+
+  private handlePlayPauseButtonHover() {
+    this.playPauseButton.addEventListener("mouseover", () => {
+      this.media.classList.add("video-embed__video-hover");
+    });
+    this.playPauseButton.addEventListener("mouseout", () => {
+      this.media.classList.remove("video-embed__video-hover");
     });
   }
 
@@ -48,9 +58,12 @@ export class VideoEmbed {
   private createPlayPauseButton() {
     const button = document.createElement("BUTTON") as HTMLButtonElement;
     button.className = "video-embed__button";
+    const triangleDiv = document.createElement("DIV") as HTMLElement;
+    triangleDiv.className = "video-embed__triangle";
     const label = document.createElement("SPAN") as HTMLElement;
     label.className = "visible-for-screen-readers";
     label.textContent = "Play Video";
+    button.appendChild(triangleDiv);
     button.appendChild(label);
     this.media = this.element.querySelector(".video-embed__media-wrap");
     this.media.appendChild(button);
