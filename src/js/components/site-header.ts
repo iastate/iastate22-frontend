5203ef9b329e2d67fe95c760bbdcaf3b275b519e
Brad Walker <brad.walker@idfive.com>
2022-03-11 16:30:09 -0500

    adding form elements - styling completed

--- src/js/components/site-header.ts
+++ src/js/components/site-header.ts
@@ -1,64 +1,17 @@
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 import * as _debounce from "lodash.debounce";
-import Micromodal from "micromodal";
 import AccessibilityUtilities from "../utilities/accessibility";
 
 const mobileMQ = window.matchMedia("(max-width: 992px)");
 
 export class SiteHeader {
   private element: HTMLElement;
-  private alertBanner: HTMLElement;
   private megaMenu: MegaMenu;
-  private siteSearch: SiteSearch;
 
   constructor(element: HTMLElement) {
     if (!!element) {
       this.element = element;
-      this.alertBanner = document.querySelector(".alert-banner");
       this.megaMenu = new MegaMenu(this.element.querySelector(".site-header__mega-menu"));
-      this.siteSearch = new SiteSearch(document.querySelector(".site-search"));
-      this.init();
-    }
-  }
-
-  private init() {
-    this.handleResize();
-    this.handleStickyNav();
-    this.handleTransitionEnd();
-  }
-
-  private handleResize() {
-    const resize = () => {
-      this.setTopOfMenu();
-    };
-    window.addEventListener("resize", _debounce(resize, 50));
-    resize();
-  }
-
-  private handleStickyNav() {
-    const headerInner = document.createElement("DIV") as HTMLElement;
-    headerInner.classList.add("site-header__inner");
-    headerInner.appendChild(this.element.querySelector(".site-header__logo"));
-    headerInner.appendChild(this.element.querySelector(".site-header__top"));
-    headerInner.appendChild(this.element.querySelector(".site-header__main"));
-    this.element.appendChild(headerInner);
-  }
-
-  private handleTransitionEnd() {
-    this.element.addEventListener("transitionend", (event) => {
-      const target = event.target as HTMLElement;
-      if (target.classList.contains("site-header__inner")) {
-        if (this.element.classList.contains("headroom--unpinned")) {
-          this.element.style.position = null;
-        }
-      }
-    });
-  }
-
-  private setTopOfMenu() {
-    if (!!this.alertBanner) {
-      const alertBannerRect = this.alertBanner.getBoundingClientRect();
-      this.element.style.top = `${alertBannerRect.bottom + window.pageYOffset}px`;
     }
   }
 }
@@ -318,81 +271,6 @@ export class MegaMenu {
       }
     } else {
       throw new Error(`Event type "${eventType}" is not allowed for MegaMenu component.`);
-    }
-  }
-}
-
-export class SiteSearch {
-  private element: HTMLElement;
-  private keywordInput: HTMLInputElement;
-  public visible: boolean = false;
-  private eventHandlers: any = {
-    show: [],
-    hide: [],
-  };
-
-  constructor(element: HTMLElement) {
-    if (!!element) {
-      this.element = element;
-      this.keywordInput = this.element.querySelector(".site-search__form-input");
-      this.init();
-    }
-  }
-
-  private init() {
-    this.handleModalShow();
-    this.handleModalClose();
-  }
-
-  /**
-   * Autofocus on search input when modal is opened
-   */
-  private handleModalShow() {
-    window.addEventListener("micromodalshow", (event: CustomEvent) => {
-      const modal = event.detail.modal as HTMLElement;
-      if (modal.id === this.element.id) {
-        setTimeout(() => {
-          this.keywordInput.focus();
-        }, 100);
-        this.visible = true;
-        this.trigger("show");
-      }
-    });
-  }
-
-  private handleModalClose() {
-    window.addEventListener("micromodalclose", (event: CustomEvent) => {
-      const modal = event.detail.modal as HTMLElement;
-      if (modal.id === this.element.id) {
-        this.visible = false;
-        this.trigger("hide");
-      }
-    });
-  }
-
-  public toggleVisibility() {
-    if (this.visible) {
-      Micromodal.close(this.element.id);
-    } else {
-      Micromodal.show(this.element.id);
-    }
-  }
-
-  public on(eventType: string, handler: Function) {
-    if (this.eventHandlers.hasOwnProperty(eventType)) {
-      this.eventHandlers[eventType].push(handler);
-    } else {
-      throw new Error(`Event type "${eventType}" is not allowed for SiteSearch component.`);
-    }
-  }
-
-  private trigger(eventType) {
-    if (this.eventHandlers.hasOwnProperty(eventType)) {
-      for (let i = 0; i < this.eventHandlers[eventType].length; i++) {
-        this.eventHandlers[eventType][i]();
-      }
-    } else {
-      throw new Error(`Event type "${eventType}" is not allowed for SiteSearch component.`);
     }
   }
 }
