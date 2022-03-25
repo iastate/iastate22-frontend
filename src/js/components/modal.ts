f0e7eb843742d5e17402ce26f126be42e08bd8f9
Brad Walker <brad.walker@idfive.com>
2022-03-25 09:56:50 -0400

    updating modal ts file to push modal content to bottom of page before closing body tag

--- src/js/components/modal.ts
+++ src/js/components/modal.ts
@@ -1,12 +1,37 @@
 import MicroModal from "micromodal";
 
-function modalTriggers() {
-  const triggers = document.querySelectorAll("[data-micromodal-trigger]");
-  for (let i = 0; i < triggers.length; i++) {
-    triggers[i].addEventListener("click", (e) => e.preventDefault());
+export class Modal {
+  private element: HTMLElement;
+  private triggers: NodeListOf<HTMLElement>;
+
+  constructor(element: HTMLElement) {
+    if (!!element) {
+      this.element = element;
+      this.triggers = document.querySelectorAll(`[data-micromodal-trigger="${this.element.id}"]`);
+      this.init();
+    }
+  }
+
+  private init() {
+    this.moveModalToBottomOfBody();
+    this.handleTriggerClicks();
+  }
+
+  private moveModalToBottomOfBody() {
+    document.body.appendChild(this.element);
+  }
+
+  private handleTriggerClicks() {
+    for (let i = 0; i < this.triggers.length; i++) {
+      this.triggers[i].addEventListener("click", (e) => e.preventDefault());
+    }
   }
 }
 
-modalTriggers();
-
-export default MicroModal;
+export default function modalsInit() {
+  const modals = document.querySelectorAll(".iastate22-modal") as NodeListOf<HTMLElement>;
+  for (let i = 0; i < modals.length; i++) {
+    new Modal(modals[i]);
+  }
+  MicroModal.init();
+}
