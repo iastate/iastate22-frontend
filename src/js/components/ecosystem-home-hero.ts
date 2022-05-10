803caa5dda02bcaaa4eff4ccca023f5523e51cf2
Tony Barsotti <tony.barsotti@idfive.com>
2022-05-10 10:34:39 -0400

    Updating ecosystem background video hero to prevent autoplay for users with reduced motion, hide fallback image for users without reduced motion

--- src/js/components/ecosystem-home-hero.ts
+++ src/js/components/ecosystem-home-hero.ts
@@ -3,32 +3,43 @@ import YoutubePlayer from "youtube-player";
 export class EcosystemHeroBackgroundVideo {
   private element: HTMLElement;
   private media: HTMLElement;
+  private playerRoot: HTMLElement;
   private player: any;
   private playButton: HTMLButtonElement;
-  private isPlaying = false;
+  private isPlaying: boolean = false;
+  private reducedMotion: boolean;
 
   constructor(element: HTMLElement) {
     if (!!element) {
       this.element = element;
-      this.init();
       this.media = this.element.querySelector(".ecosystem-home-hero__media");
+      this.playerRoot = this.element.querySelector(".ecosystem-home-hero__video");
+      this.init();
     }
   }
 
   private init() {
+    this.handlePrefersReducedMotionChange();
     this.createVideoPlayer();
     this.createPlayButton();
     this.handlePlayerEvents();
     this.handlePlayButtonClick();
   }
 
+  private handlePrefersReducedMotionChange() {
+    const reducedMotionMQ = window.matchMedia("(prefers-reduced-motion)");
+    this.reducedMotion = reducedMotionMQ.matches;
+    reducedMotionMQ.addEventListener("change", (event) => {
+      this.reducedMotion = event.matches;
+    });
+  }
+
   private createVideoPlayer() {
-    const playerRoot = this.element.querySelector(".ecosystem-home-hero__video") as HTMLElement;
-    const videoId = playerRoot.dataset.vid;
-    this.player = YoutubePlayer(playerRoot, {
+    const videoId = this.playerRoot.dataset.vid;
+    this.player = YoutubePlayer(this.playerRoot, {
       videoId,
       playerVars: {
-        autoplay: 1,
+        autoplay: this.reducedMotion ? 0 : 1, // Prevent autoplay for users with reduced motion
         controls: 0,
         disablekb: 1,
         enablejsapi: 1,
@@ -40,24 +51,31 @@ export class EcosystemHeroBackgroundVideo {
         playsinline: 1,
       },
     });
-
-    // This is necessary for autoplay on mobile
-    this.player.mute();
   }
 
   private handlePlayerEvents() {
     this.player.on("ready", () => {
+      // playerRoot has become the YouTube iframe as this point so we need a new reference to it
+      this.playerRoot = this.element.querySelector(".ecosystem-home-hero__video");
       // This is necessary for autoplay on mobile
-      this.player.playVideo();
+      this.player.mute();
+      // Only autoplay if reduced motion is not enabled
+      if (!this.reducedMotion && !this.isPlaying) {
+        this.player.playVideo();
+      }
     });
     this.player.on("stateChange", (event) => {
       if (event.data === 1) {
         this.isPlaying = true;
-        this.playButton.classList.remove("ecosystem-home-hero__video-button-play");
+        // Show the player when it initially starts playing
+        if (!this.playerRoot.style.opacity) this.playerRoot.style.opacity = "1";
+        this.playButton.querySelector(".visible-for-screen-readers").textContent = "Pause Video";
+        this.playButton.classList.add("ecosystem-home-hero__video-button--pause");
       }
       if (event.data === 2) {
         this.isPlaying = false;
-        this.playButton.classList.add("ecosystem-home-hero__video-button-play");
+        this.playButton.querySelector(".visible-for-screen-readers").textContent = "Play Video";
+        this.playButton.classList.remove("ecosystem-home-hero__video-button--pause");
       }
     });
   }
