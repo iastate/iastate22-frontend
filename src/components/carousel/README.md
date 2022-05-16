7fb9eac781c778d570ac01e9203bff5a1722da54
Brad Walker <brad.walker@idfive.com>
2022-05-16 14:15:09 -0400

    updating carousel to prefix with iastate22- to avoid bootstrap conflicts, updating readme

--- src/components/carousel/README.md
+++ src/components/carousel/README.md
@@ -1,10 +1,9 @@
 # Carousel
 
-## Design Notes
-Out of the box, the carousel component supports quick customization of the following properties:
+**This is a custom component (the class has been prefaced with `.iastate22-[classname]`).**
 
-- Aspect ratio of the slide images
-- `height`, `width`, `background-color`, and `transition` of the previous and next buttons
-- `background-color` of the previous and next buttons when hovered over
-- `color` and `transition` of the arrows in the previous and next buttons
-- `color` of the arrows in the previous and next buttons when hovering over the button
+- The carousel uses the [flickity](https://flickity.metafizzy.co/) library
+- The imag is added as a background image
+- Optional elements are the caption, title and body sections
+- The custom styles are imported into the `index.scss` file as `@import "../components/carousel/carousel";`
+- The `.ts` file for the javascript is imported into the `index.ts` file as `import carouselsInit from "./components/carousel";` and called as `carouselsInit();`
--- src/js/components/carousel.ts
+++ src/js/components/carousel.ts
@@ -12,11 +12,11 @@ export class Carousel {
   private carousel: Flickity;
 
   constructor(element: HTMLElement) {
-    if (!!element && !!element.querySelector(".carousel")) {
+    if (!!element && !!element.querySelector(".iastate22-carousel")) {
       this.element = element;
       this.elementNavNext = this.element.querySelector(".carousel__nav-next") as HTMLElement;
       this.elementNavPrevious = this.element.querySelector(".carousel__nav-previous") as HTMLElement;
-      this.carousel = new Flickity(this.element.querySelector(".carousel"), {
+      this.carousel = new Flickity(this.element.querySelector(".iastate22-carousel"), {
         contain: true,
         imagesLoaded: true,
         wrapAround: true,
@@ -102,7 +102,7 @@ export class Carousel {
 }
 
 export default function carouselsInit() {
-  const carousels = document.querySelectorAll(".carousel-holder") as NodeListOf<HTMLElement>;
+  const carousels = document.querySelectorAll(".iastate22-carousel-holder") as NodeListOf<HTMLElement>;
   for (let i = 0; i < carousels.length; i++) {
     new Carousel(carousels[i]);
   }
