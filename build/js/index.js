ca894a8050c03a05bcc34d44dcd53b0486874e0e
Mark Levy <mark.levy@idfive.com>
2024-01-11 11:30:26 -0500

    Twitter icons on the Home Social components changed to X

--- build/js/index.js
+++ build/js/index.js
@@ -76,7 +76,7 @@
   }
   /******/
   /******/ var hotApplyOnUpdate = true; // eslint-disable-next-line no-unused-vars
-  /******/ /******/ var hotCurrentHash = "6166bde7923fbf842f51";
+  /******/ /******/ var hotCurrentHash = "925fe34b8f147481670a";
   /******/ var hotRequestTimeout = 10000;
   /******/ var hotCurrentModuleData = {};
   /******/ var hotCurrentChildModule; // eslint-disable-next-line no-unused-vars
